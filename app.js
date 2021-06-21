65fb8b78ae4e033e759ffee056e52adea9a253c2
Anup Singh <anups5246@gmail.com>
2021-06-21 13:44:58 +0530

    message

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let menu = document.querySelector('.menu');
 let closeBtn = document.querySelector('#close');
 let burger = document.querySelector('#burger');
+let header = document.querySelector('.header');
 
 burger.addEventListener('click', () => {
     if (menu.classList.contains('open')) {
         menu.classList.remove('open');
         burger.style.display = "none";
         closeBtn.classList.remove('open');
+        header.classList.add('fixed');
     }
 })
 closeBtn.addEventListener('click', () => {
@@ -14,5 +16,6 @@ closeBtn.addEventListener('click', () => {
         menu.classList.add('open');
         burger.style.display = "block";
         closeBtn.classList.add('open');
+        header.classList.remove('fixed');
     }
 })
