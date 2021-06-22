1cd66d3bc2fd95a2308c66abc3bd6c32b4a5ccbe
Anup Singh <anups5246@gmail.com>
2021-06-22 15:37:01 +0530

    fixed the nav

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let menu = document.querySelector('.menu');
 let closeBtn = document.querySelector('#close');
 let burger = document.querySelector('#burger');
-let header = document.querySelector('.header');
+let body = document.querySelector('.body');
 
 burger.addEventListener('click', () => {
     if (menu.classList.contains('open')) {
         menu.classList.remove('open');
         burger.style.display = "none";
         closeBtn.classList.remove('open');
-        header.classList.add('fixed');
+        body.classList.add('fixed');
     }
 })
 closeBtn.addEventListener('click', () => {
@@ -16,6 +16,6 @@ closeBtn.addEventListener('click', () => {
         menu.classList.add('open');
         burger.style.display = "block";
         closeBtn.classList.add('open');
-        header.classList.remove('fixed');
+        body.classList.remove('fixed');
     }
 })
